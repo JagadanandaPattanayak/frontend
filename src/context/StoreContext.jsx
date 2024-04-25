1dea0d4669f6d3a9dd91be65c71e5b658f242b08
Jagadananda Pattanayak <jagadanandapattanayak97@gmail.com>
2024-04-25 18:12:40 +0530

    final commit of frontend part

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,19 +7,26 @@ const StoreContextProvider = (props) => {
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
-      setCartItems((prev)=>({...prev,[itemId]:1}));
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
-      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
   };
 
   const removeFromCart = (itemId) => {
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  useEffect(()=>{
-    console.log(cartItems);
-  }, [cartItems]);
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
 
   const contextValue = {
     food_list,
@@ -27,6 +34,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount,
   };
 
   return (
